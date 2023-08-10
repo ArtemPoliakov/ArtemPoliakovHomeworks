164f53be14b48b3af45fe84e428ee8afc962c525
ArtemPoliakov <apoliakov50@gmail.com>
2023-08-10 12:20:40 +0300

    Second commit

--- GoIT_Homeworks/src/main/java/org/example/Homework_2.java
+++ GoIT_Homeworks/src/main/java/org/example/Homework_2.java
@@ -2,3 +2,25 @@ package org.example;
 
 public class Homework_2 {
 }
+class NameEncoderDecoder{
+    public String encode(String name){
+        name = name.replace('e', '1')
+                .replace('u', '2')
+                .replace('i', '3')
+                .replace('o', '4')
+                .replace('a', '5');
+        return "NOTFORYOU" + name + "YESNOTFORYOU";
+    }
+    public String decode(String name){
+        name = name.replace('1', 'e')
+                .replace('2', 'u')
+                .replace('3', 'i')
+                .replace('4', 'o')
+                .replace('5', 'a');
+        name = name.replaceFirst("NOTFORYOU", "");
+        name = name.replace("YESNOTFORYOU", "");
+        return name;
+
+        // по-нормальному треба обрізати по індексам (ім'я теоретично може бути YESNOTFORYOU)
+    }
+}
--- GoIT_Homeworks/src/main/java/org/example/Homework_4.java
+++ GoIT_Homeworks/src/main/java/org/example/Homework_4.java
@@ -1,2 +1,19 @@
-package org.example;public class Homework_4 {
-}
+package org.example;
+
+public class Homework_4 {}
+    class ATM{
+        public int countBanknotes(int sum){
+            int[] banknoteValues = new int[]{500,200,100,50,20,10,5,2,1};
+            int totalQuantity = 0;
+            int counter = 0;
+            while(sum>0){
+                int value = banknoteValues[counter];
+                int thisQuantity = sum/value;
+                totalQuantity+=thisQuantity;
+                sum-= value*thisQuantity;
+                counter++;
+            }
+            return totalQuantity;
+        }
+    }
+
